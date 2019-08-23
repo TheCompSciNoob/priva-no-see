99dec036b5db1cb3cc18a5feb061e891f6c44eb3
TheCompSciNoob <kyroschow@gmail.com>
2019-08-23 12:38:07 -0700

    Light sensor and calibration

--- pg1/pg1.ino
+++ pg1/pg1.ino
@@ -1,20 +1,105 @@
 #include <Adafruit_CircuitPlayground.h>
 
-static void setup() {
+#define MAX_BRIGHT 1023
+#define MIN_BRIGHT 0
+
+static bool isClosed = false;
+static int lightThreshold = MAX_BRIGHT;
+static int darkThreshold = MIN_BRIGHT;
+
+void setup()
+{
     Serial.begin(9600);
     CircuitPlayground.begin();
 }
 
-static void loop() {
-    //stop program by sending in 'q'
-    static bool done = false;
-    char input = Serial.read();
-    if (input == 'q') {
-        done = true;
+void loop()
+{
+    //open or close blinds based on brightness
+    int brightness = CircuitPlayground.lightSensor();
+    Serial.print("Brightness: "); //log sensor
+    Serial.println(brightness);
+    if (brightness > (lightThreshold + darkThreshold) / 2) //theshold is average brightness
+    {
+        CircuitPlayground.redLED(true);
+        openBlinds();
+    }
+    else
+    {
+        CircuitPlayground.redLED(false);
+        closeBlinds();
     }
 
-    //continue to run if 'q' is not entered
-    if(!done) {
-        testSensors();
+    //use buttons to calibrate brightness
+    if (CircuitPlayground.leftButton())
+    {
+        calibrateDarkThreshold();
+    }
+    else if (CircuitPlayground.rightButton())
+    {
+        calibrateLightThreshold();
+    }
+
+    delay(1000); //runs every second
+}
+
+void calibrateDarkThreshold()
+{
+    darkThreshold = CircuitPlayground.lightSensor();
+    Serial.print("Dark threshold set to: ");
+    Serial.println(darkThreshold);
+    blinkRedLedThrice();
+}
+
+void calibrateLightThreshold()
+{
+    lightThreshold = CircuitPlayground.lightSensor();
+    Serial.print("Light threshold set to: ");
+    Serial.println(lightThreshold);
+    blinkRedLedThrice();
+}
+
+void openBlinds()
+{
+    if (!isClosed)
+    {
+        return;
+    }
+    isClosed = false;
+    //TODO
+}
+
+void closeBlinds()
+{
+    if (isClosed)
+    {
+        return;
+    }
+    isClosed = true;
+    //TODO
+}
+
+void toggleBlinds()
+{
+    if (isClosed)
+    {
+        openBlinds();
+    }
+    else
+    {
+        closeBlinds();
+    }
+}
+
+void blinkRedLedThrice()
+{
+    CircuitPlayground.redLED(false);
+    delay(1000);
+    for (int i = 0; i < 3; i++)
+    {
+        CircuitPlayground.redLED(true);
+        delay(200);
+        CircuitPlayground.redLED(false);
+        delay(200);
     }
 }
