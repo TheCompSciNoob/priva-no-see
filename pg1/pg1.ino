b574eeedb258cb664a9e4ae2cced44b6065b75c3
Sonika Ram <sonikaram@Sonikas-MacBook-Air-7.local>
2019-08-26 21:42:22 -0700

    Start on clap sensor

--- pg1/pg1.ino
+++ pg1/pg1.ino
@@ -8,49 +8,33 @@ static void setup() {
 }
 
 static void loop() {
-    //stop program by sending in 'q'
-    static bool done = false;
-    char input = Serial.read();
-    if (input == 'q') {
-        done = true;
-    }
-
-    //continue to run if 'q' is not entered
-    if(!done) {
-        testSensors();
-    }
+    clapSensor();
 }
 
-void printSoundLevel()
+
+
+void clapSensor()
 {
-    float value = CircuitPlayground.mic.soundPressureLevel(10);
-    Serial.print("Sound Sensor SPL: ");
-    Serial.println(value);
+  float value = CircuitPlayground.mic.soundPressureLevel(10);
+  
+  Serial.print("Sound Sensor SPL: ");
+  Serial.println(value);
+  
+  
+  if (value >= 68)
+  {
+       CircuitPlayground.redLED(HIGH);
+       delay(50);
+       CircuitPlayground.redLED(LOW);
+       delay(50);
+  }
+  
+
+ // Serial.print("Sound Sensor SPL: ");
+ // Serial.println(value2);
+
+  delay(90);
+
 }
 
-void printTemperature()
-{
-    float tempC = CircuitPlayground.temperature();
-    float tempF = CircuitPlayground.temperatureF();
-    Serial.print(tempC);
-    Serial.print(" ; ");
-    Serial.println(tempF);
-}
 
-void blinkRedLed()
-{
-    CircuitPlayground.redLED(true);
-    delay(200);
-    CircuitPlayground.redLED(false);
-    delay(200);
-    CircuitPlayground.redLED(true);
-    delay(200);
-    CircuitPlayground.redLED(false);
-    delay(1000);
-}
-
-void testSensors() {
-    blinkRedLed();
-    printTemperature();
-    printSoundLevel();
-}
